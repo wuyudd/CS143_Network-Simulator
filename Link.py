1c0f251bfb729ad3180f285124fc3cd827fb65ed
williamgu1996 <41409173+williamgu1996@users.noreply.github.com>
2018-11-19 23:36:28 -0500

    Add files via upload
    updated links

--- Link.py
+++ Link.py
@@ -1,22 +1,31 @@
-import time
-import collections
-
-
 class Link(object):
-    def __init__(self, buffer_size, delay):
-        self.delay = delay
-        self.size = buffer_size
-        self.buffer = collections.deque()
-        self.on_the_link = collections.deque()
-        
-    def add_pkg_to_buffer(self, pkg):
-        self.buffer.append(pkg)
-        
-    def transmit(self):
-        pkg = self.buffer.popleft()
-        time.sleep(self.delay)
-        self.on_the_link.append(pkg)
+	def __init__(self, transmission_delay, propagation_delay, buffer_size,start,end):
+		self.size = buffer_size
+		self.buffer = collections.deque()
+		self.on_the_link = collections.deque()
+		self.transmission_delay = transmission_delay
+		self.propagation_delay = propagation_delay
+		self.start = start
+		self.end = end
 
-    def pick_pkg_from_link(self):
-        return self.on_the_link.popleft()
+	def add_pkg_to_buffer(self, pkg):
+		if len(self.buffer) < self.size:
+			self.buffer.append(pkg)
+
+	def transmit(self):
+		pkg = self.buffer.popleft()
+		time.sleep(self.delay)
+		self.on_the_link.append(pkg)
+
+	def pick_pkg_from_link(self):
+		return self.on_the_link.popleft()
+
+
+
+
+
+
+
+
+
 
