c8d2eaa6b070eb9f28a27bd3fd5a8fb20bfb05cb
WilliamGu <williamgu@WilliamGudeMacBook-Air.local>
2018-11-22 17:15:25 -0500

    update simulator

--- simulator.py
+++ simulator.py
@@ -17,41 +17,38 @@ class Simulator(object):
         self.flow = {}
 
     def run(self):
-        node, links = self.build_graph('test1_temp.txt')
+        routers, hosts, links = self.build_graph('test1_temp.txt')
+        print(routers)
+        print(hosts)
         print(links)
-        flows = self.read_flow('flows.txt', node)
-        H1 = node['H1']
-        H2 = node['H2']
-
-# under construction
-        # node to routers
+        flows = self.read_flow('flows.txt', hosts)
+        H1 = routers['H1']
+        H2 = routers['H2']
 
         for i in range(5):
             start_time = i * global_consts.UPDATEFREQUENCY
-            event = event_type.UpdateRoutingInfo(node, start_time)
+            event = event_type.UpdateRoutingInfo(routers, start_time)
             heapq.heappush(global_var.queue, event)
 
 
         f = flows['F1']
-        node['R1'].routing_table = {"10.10.10.1": links['L0*'], "10.10.10.2": links['L5']}
-        print(node['R1'].incoming_links)
-        print(node['R1'].outgoing_links)
+        #routers['R1'].routing_table = {"10.10.10.1": links['L0*'], "10.10.10.2": links['L5']}
+        #print(routers['R1'].incoming_links)
+        #print(routers['R1'].outgoing_links)
 
         #flow = self.import_flow()
 
         # deal with the flow, add event to queue
         #for f in flow:
-        event_temp = event_type.FlowInitialize(f, f.start_time)
-        heapq.heappush(global_var.queue, event_temp)
+
+        #event_temp = event_type.FlowInitialize(f, f.start_time)
+        #heapq.heappush(global_var.queue, event_temp)
         # print(global_var.queue)
 
         while global_var.queue:
             event = heapq.heappop(global_var.queue)
             global_var.timestamp = event.start_time
             event.action()
-
-    def import_flow(self):
-        return self
 
     def build_graph(self, file_name):
         data = [line.strip('\n') for line in open(file_name, 'r')]
@@ -129,9 +126,18 @@ class Simulator(object):
                 links[cur[0] + '*'].end = node[endA]
 
             i += 1
-        return node, links
 
-    def read_flow(self, file_name, node):
+        routers = {}
+        hosts = {}
+        for key in node.keys():
+            if key[0] == 'H':
+                hosts[key] = node[key]
+            elif key[0] == 'R':
+                routers[key] = node[key]
+        return routers, hosts, links
+
+
+    def read_flow(self, file_name, hosts):
         data = [line.strip('\n') for line in open(file_name, 'r')]
         #print(data)
         flows = {}
@@ -139,7 +145,7 @@ class Simulator(object):
         while i < len(data):
             cur = data[i].split('\t')
             print(cur)
-            flows[cur[0]] = Flow(cur[0], node[cur[1]], node[cur[2]], float(cur[3]), float(cur[4]), global_consts.PACKETSIZE)
-            node[cur[1]].flows[cur[0]] = flows[cur[0]]
+            flows[cur[0]] = Flow(cur[0], hosts[cur[1]], hosts[cur[2]], float(cur[3]), float(cur[4]), global_consts.PACKETSIZE)
+            hosts[cur[1]].flows[cur[0]] = flows[cur[0]]
             i += 1
         return flows
