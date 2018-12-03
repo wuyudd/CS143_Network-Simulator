b795fde986bc88ff391f48e315203c163f5bb9f5
WilliamGu <williamgu@WilliamGudeMacBook-Air.local>
2018-12-03 17:10:04 -0500

    simplify plot

--- visulization.py
+++ visulization.py
@@ -13,79 +13,29 @@ class Visulization(object):
         # print(self.links['L1'].plot_link_buffer_time)
 
         plt.figure(figsize=(16, 16))
-        plt.suptitle('link buffer usage', fontsize=12)
-
-        plt.subplot(611)
-        plt.plot(self.links['L0'].plot_link_buffer_time, self.links['L0'].plot_link_buffer)
-        plt.xlabel('time(s)')
-        plt.ylabel('L0_Buffer')
-
-        plt.subplot(612)
-        plt.scatter(self.links['L1'].plot_link_buffer_time, self.links['L1'].plot_link_buffer, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L1_Buffer')
-        # delete it
-        '''
-        plt.subplot(613)
-        plt.scatter(self.links['L1*'].plot_link_buffer_time, self.links['L1*'].plot_link_buffer, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L1*_Buffer')
-        '''
-        plt.subplot(613)
-        plt.scatter(self.links['L2'].plot_link_buffer_time, self.links['L2'].plot_link_buffer, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L2_Buffer')
-        plt.subplot(614)
-        plt.scatter(self.links['L3'].plot_link_buffer_time, self.links['L3'].plot_link_buffer, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L3_Buffer')
-        plt.subplot(615)
-        plt.scatter(self.links['L4'].plot_link_buffer_time, self.links['L4'].plot_link_buffer, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L4_Buffer')
-        plt.subplot(616)
-        plt.scatter(self.links['L5'].plot_link_buffer_time, self.links['L5'].plot_link_buffer, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L5_Buffer')
+        plt.suptitle('link rate', fontsize=12)
+        prefix = 610
+        i = 1
+        for linkid, link in self.links.items():
+            if linkid[-1] != '*':
+                plt.subplot(prefix+i)
+                plt.scatter(link.plot_link_buffer_time, link.plot_link_buffer, s=3)
+                plt.xlabel('time(s)')
+                plt.ylabel(linkid + '_Buffer')
+                i += 1
+        plt.show()
 
         plt.figure(figsize=(16, 16))
-        #plt.suptitle('link rate', fontsize=12)
-        #plt.subplot(611)
-
-
-        plt.subplot(611)
-        plt.scatter(global_var.plot_link_rate_time_axis, self.links['L0'].plot_link_rate, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L0')
-
-        plt.subplot(612)
-        plt.scatter(global_var.plot_link_rate_time_axis, self.links['L1'].plot_link_rate, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L1')
-        # delete it
-        '''
-        plt.subplot(613)
-        plt.scatter(global_var.plot_link_rate_time_axis, self.links['L1*'].plot_link_rate, s=3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L1*')
-        '''
-        plt.subplot(613)
-        plt.scatter(global_var.plot_link_rate_time_axis, self.links['L2'].plot_link_rate, s = 3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L2')
-        plt.subplot(614)
-        plt.scatter(global_var.plot_link_rate_time_axis, self.links['L3'].plot_link_rate, s = 3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L3')
-        plt.subplot(615)
-        plt.scatter(global_var.plot_link_rate_time_axis, self.links['L4'].plot_link_rate, s = 3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L4')
-        plt.subplot(616)
-        plt.scatter(global_var.plot_link_rate_time_axis, self.links['L5'].plot_link_rate, s = 3)
-        plt.xlabel('time(s)')
-        plt.ylabel('L5')
-
+        plt.suptitle('link rate', fontsize=12)
+        prefix = 610
+        i = 1
+        for linkid,link in self.links.items():
+            if linkid[-1] != '*':
+                plt.subplot(prefix+i)
+                plt.scatter(global_var.plot_link_rate_time_axis, link.plot_link_rate, s=3)
+                plt.xlabel('time(s)')
+                plt.ylabel(linkid)
+                i += 1
         plt.show()
 
 
