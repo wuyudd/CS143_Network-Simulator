87fe1dd1c767e0f35312fa9755e63cb56bdd8d77
Yu Wu <ywwu@Yus-MacBook-Pro.local>
2018-12-11 00:38:23 -0800

    correct typo.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from simulator import *
-from visulization import *
+from visualization import *
 import global_var
 
 # link rate, link buffer usage, flow rate, flow window size, RTT
@@ -7,7 +7,7 @@ import global_var
 if __name__ == '__main__':
     sim = Simulator()
     sim_links, sim_flows = sim.run('test2.txt', 'flows2.txt')
-    vis = Visulization(sim_links, sim_flows, 'test2.txt')
+    vis = Visualization(sim_links, sim_flows, 'test2.txt')
     vis.plot_link_buffer_usage()
     vis.plot_link_rate()
     vis.plot_window_size()
