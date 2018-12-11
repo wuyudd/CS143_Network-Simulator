8232c8649662a4aef144b9f346a2b9d84a652969
WilliamGu <williamgu@WilliamGudeMacBook-Air.local>
2018-12-11 18:14:31 -0500

    update the comment for link and simulator

--- simulator.py
+++ simulator.py
@@ -12,33 +12,48 @@ import global_consts
 
 class Simulator(object):
     def __init__(self):
-        self.node = {}
+        # they are the main component of simulations
+        # nodes is a map which contains each node: key is the name of host/router and value is the instance of host/router
+        self.nodes = {}
+        # links is a map which contains each link: key is the name of link and value is the instance of host/router
         self.links = {}
-        self.flow = {}
+        # links is a map which contains each flow: key is the name of flow and value is the instance of flow
+        self.flows = {}
+    # simulation takes in the graph_file(network structure) and flow_file(flows information)
 
     def run(self, graph_file_name, flow_file_name):
+        # from the graph file, call build_graph function to reconstruct the network structure
         routers, hosts, links = self.build_graph(graph_file_name)
+        # for debug purpose, shows full layout of the newtwork structure
         self.display_graph(routers, hosts, links)
+        # from flows file, call read_flow to build flow instances
         flows = self.read_flow(flow_file_name, hosts)
 
+        # initialization of the routers, let each router knows its neighbors
         event = event_type.SayHello(routers, hosts, -3)
         heapq.heappush(global_var.queue, event)
+        # update the information for routing
         event = event_type.UpdateRoutingInfo(routers, -2)
         heapq.heappush(global_var.queue, event)
+        # initialization of check-link-rate event
         event = event_type.CheckLinkRate(links, 0)
         heapq.heappush(global_var.queue, event)
 
-        # deal with the flow, add event to queue
+        # deal with the flow, initialize each flows
         for name, f in flows.items():
             event_temp = event_type.FlowInitialize(f, f.start_time)
             heapq.heappush(global_var.queue, event_temp)
-        # print(global_var.queue)
 
+        # run the whole process
         while global_var.queue:
+            # run the event one by one
             event = heapq.heappop(global_var.queue)
             global_var.timestamp = event.start_time
             event.action()
-            queue_length = len(global_var.queue)
+
+            # following determines if all flows are end
+            # if all flows end: no need to add check-link-rate event or update-routing-info event
+            # if there is one flow still going one: still need to add check-link-rate event or update-routing-info event
             if isinstance(event, event_type.UpdateRoutingInfo) or isinstance(event, event_type.CheckLinkRate):
                 all_finished_sum = 0
                 for flow_id, cur_flow in flows.items():
@@ -51,8 +66,8 @@ class Simulator(object):
                         new_event = event_type.CheckLinkRate(links, global_var.timestamp + global_consts.READLINKRATEFREQUENCY)
                         heapq.heappush(global_var.queue, new_event)
 
+        #for debug, output the number of packets lost for each link
         for l in links.values():
-            print(l.id+' delay: '+str(l.link_delay))
             print(l.id+' lost_pkt: '+str(l.num_lost_pkt))
         return links, flows
 
@@ -60,21 +75,21 @@ class Simulator(object):
     def build_graph(self, file_name):
         data = [line.strip('\n') for line in open(file_name, 'r')]
         i = 1
-        node = {}
+        nodes = {}
         links = {}
 
         # print(data)
         # read host
         while i < len(data) and data[i] != '#':
             cur = data[i].split('\t')
-            node[cur[0]] = Host(cur[1])
+            nodes[cur[0]] = Host(cur[1])
             i += 1
         i += 1
 
         # read router
         while i < len(data) and data[i] != '#':
             cur = data[i].split('\t')
-            node[cur[0]] = Router(cur[0])
+            nodes[cur[0]] = Router(cur[0])
             i += 1
         i += 1
 
@@ -85,7 +100,7 @@ class Simulator(object):
             links[cur[0] + '*'] = Link(cur[0] + '*', float(cur[1]), float(cur[2])*(10**-3), float(cur[3]), None, None)
             i += 1
         i += 1
-        #print(node)
+        #print(nodes)
         # read graph
         while i < len(data) and data[i] != '#':
             cur = data[i].split('\t')
@@ -93,54 +108,54 @@ class Simulator(object):
             # print(cur)
             endA,endB = cur[1],cur[2]
             if endA[0] == 'H' and endB[0] == 'H':
-                node[endA].outgoing_links = links[cur[0]]
-                node[endB].incoming_links = links[cur[0]]
-                links[cur[0]].start = node[endA]
-                links[cur[0]].end = node[endB]
-                node[endA].incoming_links = links[cur[0] + '*']
-                node[endB].outgoing_links = links[cur[0] + '*']
-                links[cur[0] + '*'].start = node[endB]
-                links[cur[0] + '*'].end = node[endA]
+                nodes[endA].outgoing_links = links[cur[0]]
+                nodes[endB].incoming_links = links[cur[0]]
+                links[cur[0]].start = nodes[endA]
+                links[cur[0]].end = nodes[endB]
+                nodes[endA].incoming_links = links[cur[0] + '*']
+                nodes[endB].outgoing_links = links[cur[0] + '*']
+                links[cur[0] + '*'].start = nodes[endB]
+                links[cur[0] + '*'].end = nodes[endA]
 
             elif endA[0] == 'H' and endB[0] == 'R':
-                node[endA].outgoing_links = links[cur[0]]
-                node[endB].incoming_links[cur[0]] = links[cur[0]]
-                links[cur[0]].start = node[endA]
-                links[cur[0]].end = node[endB]
-                node[endA].incoming_links = links[cur[0] + '*']
-                node[endB].outgoing_links[cur[0]+'*'] = links[cur[0] + '*']
-                links[cur[0] + '*'].start = node[endB]
-                links[cur[0] + '*'].end = node[endA]
+                nodes[endA].outgoing_links = links[cur[0]]
+                nodes[endB].incoming_links[cur[0]] = links[cur[0]]
+                links[cur[0]].start = nodes[endA]
+                links[cur[0]].end = nodes[endB]
+                nodes[endA].incoming_links = links[cur[0] + '*']
+                nodes[endB].outgoing_links[cur[0]+'*'] = links[cur[0] + '*']
+                links[cur[0] + '*'].start = nodes[endB]
+                links[cur[0] + '*'].end = nodes[endA]
 
             elif endA[0] == 'R' and endB[0] == 'H':
-                node[endA].outgoing_links[cur[0]] = links[cur[0]]
-                node[endB].incoming_links = links[cur[0]]
-                links[cur[0]].start = node[endA]
-                links[cur[0]].end = node[endB]
-                node[endA].incoming_links[cur[0] + '*'] = links[cur[0] + '*']
-                node[endB].outgoing_links = links[cur[0] + '*']
-                links[cur[0] + '*'].start = node[endB]
-                links[cur[0] + '*'].end = node[endA]
+                nodes[endA].outgoing_links[cur[0]] = links[cur[0]]
+                nodes[endB].incoming_links = links[cur[0]]
+                links[cur[0]].start = nodes[endA]
+                links[cur[0]].end = nodes[endB]
+                nodes[endA].incoming_links[cur[0] + '*'] = links[cur[0] + '*']
+                nodes[endB].outgoing_links = links[cur[0] + '*']
+                links[cur[0] + '*'].start = nodes[endB]
+                links[cur[0] + '*'].end = nodes[endA]
 
             elif endA[0] == 'R' and endB[0] == 'R':
-                node[endA].outgoing_links[cur[0]] = links[cur[0]]
-                node[endB].incoming_links[cur[0]] = links[cur[0]]
-                links[cur[0]].start = node[endA]
-                links[cur[0]].end = node[endB]
-                node[endA].incoming_links[cur[0] + '*'] = links[cur[0] + '*']
-                node[endB].outgoing_links[cur[0] + '*'] = links[cur[0] + '*']
-                links[cur[0] + '*'].start = node[endB]
-                links[cur[0] + '*'].end = node[endA]
+                nodes[endA].outgoing_links[cur[0]] = links[cur[0]]
+                nodes[endB].incoming_links[cur[0]] = links[cur[0]]
+                links[cur[0]].start = nodes[endA]
+                links[cur[0]].end = nodes[endB]
+                nodes[endA].incoming_links[cur[0] + '*'] = links[cur[0] + '*']
+                nodes[endB].outgoing_links[cur[0] + '*'] = links[cur[0] + '*']
+                links[cur[0] + '*'].start = nodes[endB]
+                links[cur[0] + '*'].end = nodes[endA]
 
             i += 1
 
         routers = {}
         hosts = {}
-        for key in node.keys():
+        for key in nodes.keys():
             if key[0] == 'H':
-                hosts[key] = node[key]
+                hosts[key] = nodes[key]
             elif key[0] == 'R':
-                routers[key] = node[key]
+                routers[key] = nodes[key]
         return routers, hosts, links
 
     # This function read the txt file and construct the flows
@@ -156,6 +171,7 @@ class Simulator(object):
             hosts[cur[1]].flows[cur[0]] = flows[cur[0]]
             i += 1
         return flows
+
     # This function display the connection of the graph
     def display_graph(self,routers, hosts, links):
         for link in links.values():
