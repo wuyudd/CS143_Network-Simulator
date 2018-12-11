129474a5715b05eb36842680d2d7468fff5b2a25
WilliamGu <williamgu@WilliamGudeMacBook-Air.local>
2018-12-11 18:29:31 -0500

    update comment for event/global_consts/global_var

--- global_var.py
+++ global_var.py
@@ -1,7 +1,15 @@
+# queue is a priority queue to store the event
 queue = []
+# timestamp is a timer globally
 timestamp = float(0.0)
+# period is used for update the routing information
+# in each period, each router will only accept the routing information sent from other routers in current period
 period = 0
+# updating_flag is a boolean
+# when it is True: each router has the authorization to receive the information from other routers
+# when it is False: each router has NO authorization to receive the information from other routers
 updating_flag = False
 
+# following global variable is for plotting
 plot_link_rate_time_axis = []
 plot_window_size_pkt_timestamp = []
