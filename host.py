bcd2c5545879aa0c31d9984133ecb672b6eea665
Yu Wu <ywwu@Yus-MacBook-Pro.local>
2018-12-11 16:14:53 -0800

    add a little bit commenting.

--- host.py
+++ host.py
@@ -13,8 +13,10 @@ class Host(Node):
         self.flows = {}
         self.incoming_links = None
         self.outgoing_links = None
-        self.neighbors = {}  # key is the neighbor node, value is the outgoing_links
-        self.flow_lost_pkt_pointer = {} # key is the corresponding flow, value is the first 0's position(this position's pkt has not been acknowleged) in the recieved_pkts_data list
+        # key is the neighbor node, value is the outgoing_links
+        self.neighbors = {}
+        # key is the corresponding flow, value is the first 0's position(this position's pkt has not been acknowleged) in the recieved_pkts_data list
+        self.flow_lost_pkt_pointer = {}
         self.send_flag = 0
         self.recieved_pkts_data = []
         # a list to record whether a pck is received, 0: not received, 1: received
@@ -28,13 +30,12 @@ class Host(Node):
             flow_id = pkt.id.split('pkt')[0]
             self.flow_lost_pkt_pointer[flow_id] = self.flow_lost_pkt_pointer.get(flow_id, -1)
             pkt_ind = int(pkt.id.split('pkt')[1])
-
             # this pkt has already been recorded in the recieved_pkts_data list, update it's state to be received(value to be 1)
             if pkt_ind < len(self.recieved_pkts_data):
                 self.recieved_pkts_data[pkt_ind] = 1
             else:
                 i = len(self.recieved_pkts_data)
-                #set the state of the pkts between the last one in recieved_pkts_data list and the one we received to be unreceived(value to be 0)
+                # set the state of the pkts between the last one in recieved_pkts_data list and the one we received to be unreceived(value to be 0)
                 while i < pkt_ind:
                     self.recieved_pkts_data.append(0)
                     i += 1
